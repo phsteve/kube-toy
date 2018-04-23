a2718c9cbcc36ad9aa3310716c4da704dfef9bbb
Steve Katz <steve@teacherspayteachers.com>
2018-04-23 15:22:59 -0400

    add fancy feature

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world!!!!!'
+    return 'hello world!!!!! we just built an image from a new commit'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
