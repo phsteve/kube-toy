d1e70ed5bdc49c3ae014a27868fdc3e7544ff33a
Steve Katz <steve@teacherspayteachers.com>
2018-04-26 10:15:45 -0400

    Important enhancement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world!!!!! we just built an image from a new commit'
+    return 'wow it works'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
