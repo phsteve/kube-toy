67ba33f1a4d1f3068d37a7b189f75e4a1490bce1
Steve Katz <steve@teacherspayteachers.com>
2018-05-21 16:10:51 -0400

    use kubernetes service url

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.get('super-service.default.svc.cluster.local/api/some_endpoint').json()
+    response = requests.get('http://super-service.default.svc.cluster.local/api/some_endpoint').json()
     return 'wow it works. Response was: {}'.format(response['data'])
 
 @app.route('/healthcheck')
-def index():
+def healthcheck():
     return 'hello kubernetes'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
